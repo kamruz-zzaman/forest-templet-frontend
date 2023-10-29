c9554edd40f959763d1bf32f088e899d809a7f56
kamruz-zzaman <kamruz.zaman8991@gmail.com>
2023-10-30 00:24:55 +0600

    initiate repo

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "./App.css";
+import "leaflet/dist/leaflet.css";
+
+function App() {
+  const position = [23.7451003, 90.405864];
+
+  return (
+    <div>
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+}
+
+export default App;
