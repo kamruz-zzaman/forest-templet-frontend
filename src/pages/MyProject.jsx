d0809d5b092f7a514dcbed26f75399f15c5e6df6
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2023-11-11 06:06:51 +0600

    done

--- src/pages/MyProject.jsx
+++ src/pages/MyProject.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import img1 from "../assets/1.jpg";
 import img2 from "../assets/2.jpg";
@@ -6,6 +6,7 @@ import img3 from "../assets/3.jpg";
 import { Link } from "react-router-dom";
 
 const MyProject = () => {
+  const [remaining, setRemaining] = useState(false);
   return (
     <section>
       <Header />
@@ -28,17 +29,43 @@ const MyProject = () => {
             <Link to="/?scrollTo=timber" className="text-lg px-3">
               How to identify illegal timber?
             </Link>
-            <Link to="/?scrollTo=timber" className="mt-5 px-5 block">
+            <div className="mt-5 px-5 ">
               Stopping the trade in illegal timber is imperative for several
               reasons. Firstly, it helps preserve our global forests, which are
-              crucial for maintaining biodiversity, regulating climate, and
-              supporting the livelihoods of local communities. Illegal logging
-              often involves destructive practices that contribute to
-              deforestation, threatening the delicate balance of ecosystems.
-              Identifying illegal timber is essential to combat this issue
-              effectively. By doing so, we can disrupt the illicit supply chains
-              and hold responsible parties accountable.
-            </Link>
+              crucial for maintaining biodiversity, regulating climate,{" "}
+              {!remaining && (
+                <button
+                  className="text-blue-100"
+                  onClick={() => setRemaining(true)}
+                >
+                  Read More...
+                </button>
+              )}
+              {remaining
+                ? " and supporting the livelihoods of local communities. Illegal logging often involves destructive practices that contribute to deforestation, threatening the delicate balance of ecosystems. Identifying illegal timber is essential to combat this issue effectively. By doing so, we can disrupt the  illicit supply chains and hold responsible parties accountable."
+                : null}
+              {remaining ? (
+                <div className="  inline">
+                  <button className="ms-2" onClick={() => setRemaining(false)}>
+                    Read Less{" "}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      width="24"
+                      height="24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      className="inline  -ms-1"
+                    >
+                      <polyline points="18 15 12 9 6 15"></polyline>
+                    </svg>
+                  </button>
+                </div>
+              ) : null}
+            </div>
           </div>
           <div className="col-span-4 bg-primary text-center text-white py-20 h-max">
             <Link to="/?scrollTo=unsolved">Ongoing alerts</Link>
